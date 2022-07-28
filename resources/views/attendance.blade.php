8ad6af64ed57a8006a8f4721c04700f2bbb14d9c
Mehal <mehalsakthi@gmail.com>
2022-07-28 15:59:57 +0530

    dc

--- resources/views/attendance.blade.php
+++ resources/views/attendance.blade.php
@@ -32,21 +32,7 @@
 
                 <div class="container">
 
-                    {{--  <form id="myForm">
-                       <div class="form-group">
-                         <label for="name">Name:</label>
-                         <input type="text" class="form-control" id="name">
-                       </div>
-                       <div class="form-group">
-                         <label for="type">Type:</label>
-                         <input type="text" class="form-control" id="type">
-                       </div>
-                       <div class="form-group">
-                          <label for="price">Price:</label>
-                          <input type="text" class="form-control" id="price">
-                        </div>
-                       <button class="btn btn-primary" id="ajaxSubmit">Submit</button>
-                     </form>  --}}
+
                  </div>
                  <script src="http://code.jquery.com/jquery-3.3.1.min.js"
                           integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
@@ -74,11 +60,7 @@
 
                                     arrData.push(obj);
                             });
-                                //alert(arrData);
-                            //console.log(arrData);
-
-
-
+                              s
                           $.ajaxSetup({
                              headers: {
                                  'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
@@ -110,18 +92,6 @@
                           });
                        });
                  </script>
-
-
-
-                {{--  @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif  --}}
 
                 <h5 class="card-header">Mark presence of employees:</h5>
                 <div class="container">
--- resources/views/userdisp.blade.php
+++ resources/views/userdisp.blade.php
@@ -5,8 +5,25 @@
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <title>Employee Manager | Home</title>
+        <style>
+            .print {
+                display:none;
+            }
+            .no-print{
+                display:block;
+            }
+            @media print{
+                .print {
+                    display:block;
+                }
+                .no-print{
+                    display:none;
+                }
+            }
+        </style>
     </head>
     <body>
+
         {{--  {{ $msals }}  --}}
         {{--  {{ $users }}  --}}
         <div class="container my-5 mx-4">
@@ -24,10 +41,76 @@
                 <div class=" mx-4 my-2">
                     <p class="font-weight-bold m-3">Total No. of employees: {{ sizeof($users) }}</p>
                     <a class="btn btn-info mt-1 ml-3" href="msal">Calculate Monthly Salary</a>
+                    <script>
+                        function printDiv() {
+                            //Get the HTML of div
+                            var divElements = document.getElementById("printabletable").innerHTML;
+                            //Get the HTML of whole page
+                            var oldPage = document.body.innerHTML;
+                            //Reset the page's HTML with div's HTML only
+                            document.body.innerHTML =
+                            "<html><head><title></title></head><body>" +
+                            divElements + "</body>";
+                            //Print Page
+                            window.print();
+                            //Restore orignal HTML
+                            document.body.innerHTML = oldPage;
+
+                        }
+
+                    function exportDiv(){
+                        var table = document.getElementById("indextable");
+                        var rows =[];
+                        for(var i=0,row; row = table.rows[i];i++){
+                            column1 = row.cells[0].innerText;
+                            column2 = row.cells[1].innerText;
+                            column3 = row.cells[2].innerText;
+                            column4 = row.cells[3].innerText;
+                            column5 = row.cells[4].innerText;
+                            column6 = row.cells[5].innerText;
+
+                        /* add a new records in the array */
+                            rows.push(
+                                [
+                                    column1,
+                                    column2,
+                                    column3,
+                                    column4,
+                                    column5,
+                                    column6
+
+                                ]
+                            );
+
+                            }
+
+
+                            console.log(rows);
+
+                            csvContent = "data:text/csv;charset=utf-8,";
+                             /* add the column delimiter as comma(,) and each row splitted by new line character (\n) */
+                            rows.forEach(function(rowArray){
+                                row = rowArray.join(",");
+                                csvContent += row + "\r\n";
+                            });
+
+                            /* create a hidden <a> DOM node and set its download attribute */
+                            var encodedUri = encodeURI(csvContent);
+                            var link = document.createElement("a");
+                            link.setAttribute("href", encodedUri);
+                            link.setAttribute("download", "Employeedetails.csv");
+                            document.body.appendChild(link);
+                             /* download the data file named "Stock_Price_Report.csv" */
+                            link.click();
+                    }
+
+
+                </script>
+
                     </div>
-                        <div class="p-3 ">
-                        <div class="table-responsive ">
-                            <table class="table text-center">
+                        <div class="p-3 " id="printabletable">
+                        <div class="table-responsive" >
+                            <table class="table text-center" id="indextable">
                                 <thead>
                                 <tr>
                                     <th scope="col">#</th>
@@ -76,7 +159,24 @@
                                 </tbody>
                             </table>
                         </div>
+
+                    <div class="text-right mr-3">
+                        <button class="btn btn-dark mt-1 ml-1 " onclick="printDiv()">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-printer-fill" viewBox="0 0 16 16">
+                            <path d="M5 1a2 2 0 0 0-2 2v1h10V3a2 2 0 0 0-2-2H5zm6 8H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z"/>
+                            <path d="M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/>
+                            </svg>
+                        </button>
+                        <button class="btn btn-dark mt-1 ml-1 " onclick="exportDiv()">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+                                <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+                                <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+                              </svg>
+                        </button>
+
                     </div>
+                    </div>
+
                 </div>
             </div>
         </div>
